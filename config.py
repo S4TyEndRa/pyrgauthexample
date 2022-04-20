af04db36149f5d4842527b2d51c7d2c782fd5fb8
SaTyEndRa <84174959+S4TyEndRa@users.noreply.github.com>
2022-04-20 08:47:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class var():
-   APP_ID = os.getenv('APP_ID')
-   API_HASH = os.getenv('API_HASH')
-   BOT_TOKEN = os.getenv('BOT_TOKEN')
-   APP_DOMAIN = os.getenv('APP_DOMAIN')
-   PORT = os.getenv('PORT', 6969)
+   APP_ID = 2171111
+   API_HASH = 'fd7acd07303760c52dcc0ed8b2f73086'
+   BOT_TOKEN = '5217702275:AAH0KwikoXAzjEc7qO8V9XiH1-gnTAVnFF4'
+   APP_DOMAIN = 'http://satyendra.eu.org'
+   PORT = 6969
