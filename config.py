a642e63c3d43d8209b189e656080a97d36dad2cf
SaTyEndRa <84174959+S4TyEndRa@users.noreply.github.com>
2022-04-20 08:56:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ class var():
    APP_ID = 2171111
    API_HASH = 'fd7acd07303760c52dcc0ed8b2f73086'
    BOT_TOKEN = '5217702275:AAH0KwikoXAzjEc7qO8V9XiH1-gnTAVnFF4'
-   APP_DOMAIN = 'http://satyendra.eu.org/api/'
+   APP_DOMAIN = 'https://program24x.eu.org/js'
    PORT = 6969
